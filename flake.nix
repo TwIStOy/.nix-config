10b11ad6dbd5a8d1fa2787efa2a6e56fccf2cdb7
Hawtian Wang <hawtian.wang@twistoy.cn>
2024-01-22 18:55:34 +0800

    Init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Hawtian's nix configuration for both macos and linux"; 
+
+  inputs = {
+    # Official NixOS package source, using nixos's stable branch by default
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    # for macos
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with the
+      # `inputs.nixpkgs` of the current flake, to avoid problems caused by
+      # different versions of nixpkgs dependencies.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
---
+++ home/common/core.nix
@@ -0,0 +1,11 @@
+{ username, ... }:
+{
+  home = {
+    inherit username;
+
+    stateVersion = "23.11";
+  }
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
