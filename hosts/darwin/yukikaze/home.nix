d553f22197e74abb9f5a1450868781038a9a2686
Hawtian Wang <twistoy.wang@gmail.com>
2024-01-30 22:03:12 +0800

    ...

--- hosts/darwin/yukikaze/home.nix
+++ hosts/darwin/yukikaze/home.nix
@@ -1,3 +1,17 @@
 {
   programs.kitty.settings.font_size = 18;
+
+  programs.ssh = {
+    extraConfig = ''
+      Host poi
+        Hostname 192.168.50.226
+        user hawtian
+        ForwardAgent yes
+        KeepAlive yes
+        TCPKeepAlive yes
+        ServerAliveInterval 30
+        ForwardX11 yes
+        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+    '';
+  };
 }
