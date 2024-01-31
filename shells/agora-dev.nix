012108774f56ae1e7d96476dddce9f5c66d16a60
Hawtian Wang <twistoy.wang@gmail.com>
2024-01-31 23:20:02 +0800

    ...

--- shells/agora-dev.nix
+++ shells/agora-dev.nix
@@ -6,17 +6,28 @@
   pkgs = inputs.nixpkgs.legacyPackages.${system};
 in
   pkgs.mkShell rec {
-    buildInputs = with pkgs; [
+    packages = with pkgs; [
       just
       stdenv.cc.cc
       cmake
       python3
+      pkg-config
+      llvmPackages_17.clang-unwrapped
+
+      # libraries
+      abseil-cpp
+      range-v3
+      glibc
     ];
 
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
+    CPPFLAGS = "-isystem ${pkgs.abseil-cpp}/include -isystem ${pkgs.range-v3}/include";
+    AGORA_CC_PATH = "${pkgs.stdenv.cc.cc}";
+    AGORA_CC_VERSION = "${pkgs.stdenv.cc.cc.version}";
+    AGORA_PLATFORM = "${pkgs.stdenv.hostPlatform.config}";
 
     shellHook = ''
-      echo "Agora-dev"
+      echo "Agora-dev ${pkgs.abseil-cpp} ${pkgs.range-v3} ${pkgs.glibc.dev}"
       exec fish
     '';
   }
