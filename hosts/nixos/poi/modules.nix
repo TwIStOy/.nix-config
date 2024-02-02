1a5c905baf6555e6a80e96466d801357f509d261
Hawtian Wang <hawtian.wang@twistoy.cn>
2024-02-02 16:11:45 +0800

    ...

--- hosts/nixos/poi/modules.nix
+++ hosts/nixos/poi/modules.nix
@@ -1,5 +1,7 @@
-_: let
+{secrets-hawtian, ...}: let
   hostname = "poi";
+
+  frp-secrets = builtins.fromJSON (builtins.readFile "${secrets-hawtian}./frp-secrets.json");
 in {
   imports = [
     ./hardware-configuration.nix
@@ -22,6 +24,25 @@ in {
   services.frp = {
     enable = true;
     role = "client";
+    settings = {
+      inherit (frp-secrets) serverAddr serverPort auth;
+      proxies = [
+        {
+          name = "poi-host-ssh";
+          type = "tcp";
+          localIP = "127.0.0.1";
+          localPort = 22;
+          remotePort = 4000;
+        }
+        {
+          name = "poi-ssh-p2p";
+          type = "xtcp";
+          secretKey = "hawtian";
+          localIP = "127.0.0.1";
+          localPort = 22;
+        }
+      ];
+    };
   };
 
   system.stateVersion = "23.11";
