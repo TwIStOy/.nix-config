a137d0a52e08ce51258e92363379d11a7b02d782
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-04 23:25:52 +0800

    add my cache

--- modules/common.nix
+++ modules/common.nix
@@ -21,10 +21,12 @@
 
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://twistoy.cachix.org"
       "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "twistoy.cachix.org-1:74RMpK2e+jG514O3MC6KiTb9eJi4pddEnN5h6PBP0aY="
     ];
     builders-use-substitutes = true;
   };
