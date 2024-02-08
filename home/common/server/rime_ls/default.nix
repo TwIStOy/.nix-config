cca63f621c88e11dbaf98c6b7d5e1597938ca882
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-08 20:27:15 +0800

    update rime-ls

--- home/common/server/rime_ls/default.nix
+++ home/common/server/rime_ls/default.nix
@@ -1,39 +1,14 @@
 {
-  lib,
   pkgs,
-  system,
   nur-hawtian,
   ...
-}: let
-  isDarwin = lib.strings.hasSuffix "darwin" system;
-in {
+}: {
   home.packages = [
     nur-hawtian.packages.${pkgs.system}.rime-ls
   ];
-
-  home.file =
-    if isDarwin
-    then {
-      "Library/Rime" = {
-        enable = true;
-        recursive = true;
-        source = ./files;
-      };
-    }
-    else {};
 
   xdg.dataFile.rime-ls-files = {
     source = ./files;
     recursive = true;
   };
-
-  xdg.configFile =
-    if isDarwin
-    then {}
-    else {
-      "ibus/rime" = {
-        source = ./files;
-        recursive = true;
-      };
-    };
 }
