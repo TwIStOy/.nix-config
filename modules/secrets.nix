5909c97ee04215f67912ff2f7acb76a578a25b40
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-14 23:10:11 +0800

    ...

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -5,29 +5,27 @@
   username,
   agenix,
   ...
-}:
-# let
-#   # ageSecret = {
-#   #   file,
-#   #   owner ? "root",
-#   #   mode ? "400",
-#   # }: {
-#   #   file = "${hyperSelf}/secrets/${file}";
-#   #   inherit owner mode;
-#   # };
-# in 
-{
+}: let
+  ageSecret = {
+    file,
+    owner ? "root",
+    mode ? "400",
+  }: {
+    file = "${hyperSelf}/secrets/${file}";
+    inherit owner mode;
+  };
+in {
   environment.systemPackages = [
     agenix.packages."${pkgs.system}".default
   ];
 
   age.identityPaths = [
     "/etc/ssh/ssh_host_ed25519_key"
-    "${config.users.users.${username}}/.ssh/id_ed25519"
+    "${config.users.users.${username}.home}/.ssh/id_ed25519"
   ];
 
   age.secrets = {
-    frp-server-auth = {
+    frp-server-auth = ageSecret {
       file = "${hyperSelf}/secrets/frp-server-auth.age";
       owner = username;
     };
