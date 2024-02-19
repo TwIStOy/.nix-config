429c80217a20379230ae607b0b41a07a53ca614d
Hawtian Wang <hawtian.wang@twistoy.cn>
2024-02-19 11:00:36 +0800

    add yamato public key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -7,17 +7,20 @@ let
   # hosts
   poi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIChh+2JjsSms14xR5l8y2zzyCI5ryzEzeuq9N8wsaEqK";
   yukikaze = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIY8aMFW3F68el/LQyZtONVuEMwJfejRvRdjLTps84f3";
+  yamato = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID52VHi3XJl9hfHC/ywRrjV05bKqRvKulbzud/59ekX7";
 
   # collections
-  desktop = [
+  desktops = [
     yukikaze
+    yamato
   ];
-  servers = [
+  homeServers = [
     poi
   ];
 
   # helpers
   mkSecrets = list: list ++ user;
 in {
-  "frp-server-auth.age".publicKeys = mkSecrets (servers ++ desktop);
+  "frp-server-auth.age".publicKeys = mkSecrets (homeServers ++ desktops);
+  # "ssh-config-secrets.age".publicKeys = mkSecrets (homeServers ++ desktops);
 }
