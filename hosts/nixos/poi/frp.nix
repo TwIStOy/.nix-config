6ceed608324a4f0ba005a328c7f54dda2e04bc3c
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-19 21:47:00 +0800

    ...

--- hosts/nixos/poi/frp.nix
+++ hosts/nixos/poi/frp.nix
@@ -9,7 +9,6 @@
   ];
 
   imports = [
-    # "${pkgs-unstable.path}/nixos/modules/services/networking/frp.nix"
     "${nur-hawtian}/modules/services/networking/frp.nix"
   ];
 
@@ -41,26 +40,5 @@
     package = pkgs-unstable.frp;
     role = "client";
     configFile = "/etc/frp/frp-server-auth.toml";
-    # settings = {
-    #   includes = [
-    #     "/etc/frp/frp-server-auth.toml"
-    #   ];
-    #   proxies = [
-    #     {
-    #       name = "poi-host-ssh";
-    #       type = "tcp";
-    #       localIP = "127.0.0.1";
-    #       localPort = 22;
-    #       remotePort = 4002;
-    #     }
-    #     {
-    #       name = "poi-ssh-p2p";
-    #       type = "xtcp";
-    #       secretKey = "hawtian";
-    #       localIP = "127.0.0.1";
-    #       localPort = 22;
-    #     }
-    #   ];
-    # };
   };
 }
