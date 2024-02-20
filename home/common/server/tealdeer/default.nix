1274c3bff71e202d436126615afaf82d3599ac0a
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-20 22:12:43 +0800

    fix: isDarwin checking

--- home/common/server/tealdeer/default.nix
+++ home/common/server/tealdeer/default.nix
@@ -1,21 +1,26 @@
 {
   pkgs,
   lib,
+  system,
   ...
 }: let
   settingsFormat = pkgs.formats.toml {};
   configFile = settingsFormat.generate "config.toml" {
     updates.auto_update = true;
   };
+  isDarwin = lib.strings.hasSuffix "darwin" system;
 in
   {
     home.packages = with pkgs; [
       tealdeer
     ];
   }
-  // lib.attrsets.optionalAttrs (!pkgs.stdenv.isDarwin) {
-    xdg.configFile."tealdeer/config.toml".source = configFile;
-  }
-  // lib.attrsets.optionalAttrs pkgs.stdenv.isDarwin {
-    home.file."Library/Application Support/tealdeer/config.toml".source = configFile;
-  }
+  // (
+    if isDarwin
+    then {
+      home.file."Library/Application Support/tealdeer/config.toml".source = configFile;
+    }
+    else {
+      xdg.configFile."tealdeer/config.toml".source = configFile;
+    }
+  )
