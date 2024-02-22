07ef911eedd5d4a30daeb5a77ac42fbe9726e105
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-22 14:53:22 +0800

    feat: add direnv

---
+++ home/common/server/direnv/default.nix
@@ -0,0 +1,12 @@
+{...}: {
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+    };
+  };
+}
