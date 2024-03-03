282415f1c168a4e8a08f66f07820299d9fbba51c
Hawtian Wang <twistoy.wang@gmail.com>
2024-03-03 19:29:32 +0800

    feat: generate nix-aware clangd config

--- home/common/server/development/cpp.nix
+++ home/common/server/development/cpp.nix
@@ -2,10 +2,28 @@
   pkgs,
   nur-hawtian,
   ...
-}: {
+}: let
+  gcc = pkgs.gcc;
+  nixAwareClangdConfig = pkgs.stdenv.mkDerivation {
+    name = "nix-aware-clangd-config";
+
+    buildInputs = [gcc];
+
+    dontUnpack = true;
+    phases = ["installPhase"];
+
+    installPhase = ''
+      mkdir -p $out/dist
+      ($CXX -xc++ -E -v /dev/null) 2>&1 | awk 'BEGIN { incsearch = 0} /^End of search list/ { incsearch = 0 } { if(incsearch) { print $0 }} /^#include </ { incsearch = 1 }' | sed 's/^[[:space:]]*\(.*\)/"-isystem","\1"/' | tr '\n' ' ' | tr ' ' ',' | sed 's/,$//' > $out/dist/extra_args
+    '';
+  };
+in {
   home.packages =
-    (with pkgs; [
+    [
       gcc
+      nixAwareClangdConfig
+    ]
+    ++ (with pkgs; [
       cmake
       cmake-language-server
       llvmPackages_17.clang-unwrapped
@@ -13,4 +31,16 @@
     ++ [
       nur-hawtian.packages.${pkgs.system}.gersemi
     ];
+
+  # generate clangd user configuration file
+  xdg.configFile."clangd/config.yaml" = {
+    text = ''
+      CompileFlags:
+        Compiler: ${gcc}/bin/g++
+        Add: [${builtins.readFile "${nixAwareClangdConfig}/dist/extra_args"}]
+      Diagnostics:
+        Suppress: "builtin_definition"
+    '';
+    force = true;
+  };
 }
