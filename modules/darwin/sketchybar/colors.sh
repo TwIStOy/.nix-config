af1bf86cb5c7348126a9d0ae07bdbdb96812a81a
Hawtian Wang <twistoy.wang@gmail.com>
2024-03-09 23:07:42 +0800

    feat: update color

--- modules/darwin/sketchybar/colors.sh
+++ modules/darwin/sketchybar/colors.sh
@@ -3,14 +3,14 @@
 export WHITE=0xffffffff
 
 # -- Teal Scheme --
-export BAR_COLOR=0xff001f30
-export ITEM_BG_COLOR=0xff003547
-export ACCENT_COLOR=0xff2cf9ed
+# export BAR_COLOR=0xff001f30
+# export ITEM_BG_COLOR=0xff003547
+# export ACCENT_COLOR=0xff2cf9ed
 
 # -- Gray Scheme --
-# export BAR_COLOR=0xff101314
-# export ITEM_BG_COLOR=0xff353c3f
-# export ACCENT_COLOR=0xffffffff
+export BAR_COLOR=0xff101314
+export ITEM_BG_COLOR=0xff353c3f
+export ACCENT_COLOR=0xffffffff
 
 # -- Purple Scheme --
 # export BAR_COLOR=0xff140c42
