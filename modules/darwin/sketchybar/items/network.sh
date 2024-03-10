03fa1e943b73ce8ef71ecbbb4640e76db7a17fc6
Hawtian Wang <twistoy.wang@gmail.com>
2024-03-10 20:26:46 +0800

    feat: remote sketchybar

---
+++ modules/darwin/sketchybar/items/network.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+network_down=(
+	y_offset=-7
+	label.font="$FONT:Heavy:10"
+	label.color="$TEXT"
+	icon="$NETWORK_DOWN"
+	icon.font="$NERD_FONT:Bold:16.0"
+	icon.color="$GREEN"
+	icon.highlight_color="$BLUE"
+	update_freq=1
+	script="$PLUGIN_DIR/plugins/network.sh"
+)
+
+network_up=(
+	background.padding_right=-70
+	y_offset=7
+	label.font="$FONT:Heavy:10"
+	label.color="$TEXT"
+	icon="$NETWORK_UP"
+	icon.font="$NERD_FONT:Bold:16.0"
+	icon.color="$GREEN"
+	icon.highlight_color="$BLUE"
+	update_freq=1
+	script="$PLUGIN_DIR/plugins/network.sh"
+)
+
+sketchybar 	--add item network.down right 						\
+						--set network.down "${network_down[@]}" 	\
+						--add item network.up right 							\
+						--set network.up "${network_up[@]}"
